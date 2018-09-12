c6bf6abde4ac3266a46e5245fec4434835d397fc
Arquitectura <arquitectura@MacBook-Air-de-LBKITEquipamiento.local>
2018-09-12 09:23:36 +0200

    unidad 6_3

--- unidad6/src/app/formularios/formularios.component.ts
+++ unidad6/src/app/formularios/formularios.component.ts
@@ -9,16 +9,20 @@ import { Persona} from '../modelo/persona';
 export class FormulariosComponent implements OnInit {
 
   sexos: string[] = ['Hombre', 'Mujer', 'Otros'];
-  orientaciones: string[] = ['Hombre', 'Mujer', 'Otros'];
-
+  orientaciones: string[] = ['Hetero', 'Gay', 'Lesbiana', 'Otros'];
+  relaciones: string[] = ['Amistad', 'Relación estable', 'Ir al bar', 'Otros'];
+  submitted: boolean;
   model: Persona;
+  personas: Persona[] = [];
   constructor() {
     this.model = new Persona('', '', '', '', '', '', '', false);
+    this.submitted = false;
   }
 
   ngOnInit() {
   }
 
   submit() {
+    this.personas.push( this.model);
   }
 }
