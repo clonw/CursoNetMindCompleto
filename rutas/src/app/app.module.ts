59218c78bb7a15fd004bf15d4e0338fa21dfc353
Arquitectura <arquitectura@MacBook-Air-de-LBKITEquipamiento.local>
2018-09-12 11:01:10 +0200

    Unidad 7_2

--- rutas/src/app/app.module.ts
+++ rutas/src/app/app.module.ts
@@ -1,14 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ListadoComponent } from './listado/listado.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'about' , component: AboutComponent },
+  { path: 'login' , component: LoginComponent },
+  { path: 'listado' , component: ListadoComponent },
+  { path: '' , redirectTo: '/home', pathMatch: 'full' }
+ ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ListadoComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true } // <-- debugging purposes only
+      )
   ],
   providers: [],
   bootstrap: [AppComponent]
